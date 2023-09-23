ec8401530bbe1fff540c4c6dfbec0d62a015496f
Shion1314 <jiaxinlin200204@gmail.com>
2023-09-22 23:43:36 -0400

    some()

--- myEvery.js
+++ myEvery.js
@@ -1,21 +1,20 @@
-const mySome=(arr,user) =>{
-    for (let i = 0; i < arr.length; i++){
-        if(arr[i]==user){
-            return true;
-        }
+const mySome = (arr, callback) => {
+    var result = true
+    for (let i = 0; i < arr.length; i++) {
+      if (callback(arr[i], i, arr) !== true) {
+        result=false;
+      }
     }
-    return false;
-
-}
-
+    return result;
+  };
+  
   const input = document.getElementById("input");
   const output = document.getElementById("output");
   const button_1 = document.getElementById("button_1");
   const user = document.getElementById("User_input")
-
   button_1.addEventListener('click', () => {
   
-    const result = mySome(input.value, user.value);
+    const result = mySome(input.value, (val) => val === user.value);
     output.value = result;
   });
   
