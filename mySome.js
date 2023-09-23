341ed4c19e63176f7f45b3d23d93496011f0c5dd
Shion1314 <jiaxinlin200204@gmail.com>
2023-09-22 23:44:58 -0400

    some()

--- mySome.js
+++ mySome.js
@@ -1,19 +1,20 @@
-const mySome = (arr, callback) => {
-    for (let i = 0; i < arr.length; i++) {
-      if (callback(arr[i], i, arr) === true) {
-        return true;
-      }
+const mySome=(arr,user) =>{
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i]==user){
+            return true;
+        }
     }
     return false;
-  };
-  
+
+}
+
   const input = document.getElementById("input");
   const output = document.getElementById("output");
   const button_1 = document.getElementById("button_1");
   const user = document.getElementById("User_input")
   button_1.addEventListener('click', () => {
   
-    const result = mySome(input.value, (val) => val === user.value);
+    const result = mySome(input.value, user.value);
     output.value = result;
   });
   
